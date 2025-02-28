1ee2763c9e29e608dccba157b1b58cb3a9284618
LaryR2007 <laryrenderos2007@gmail..com>
2025-02-28 12:53:08 -0500

    Lary and Erij

--- mysite/todo/urls.py
+++ mysite/todo/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'todo'
 
 urlpatterns = [
-    path('items', views.TodoListView.as_view(), name='items'),
-    path('item/<int:pk_from_url>', views.TodoDetailView.as_view(), name='item'),
+    path('', views.TodoListView.as_view(), name='items'),
+    path('<int:pk_from_url>', views.TodoDetailView.as_view(), name='item'),
     path('add/', views.AddItemView.as_view(), name='add_item'),
     path(
         '<int:pk>/delete/',
