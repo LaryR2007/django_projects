ac0fb51b10157781ba06b88bef78476024eb9530
LaryR2007 <laryrenderos2007@gmail..com>
2025-03-28 20:11:40 -0400

    ran into some errors

--- mysite/books/urls.py
+++ mysite/books/urls.py
@@ -4,11 +4,13 @@ from .views import (BookListView, BookDetailView, BookCreateView, BookUpdateView
 
 app_name = 'books'
 urlpatterns = [
-    path('', views.BookListView.as_view(), name='book_list'),
+    path('', views.GenreListView.as_view(), name='genre_list'),
     path('book/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
     path('book/new/', views.BookCreateView.as_view(), name='book_create'),
     path('book/<int:pk>/edit/', views.BookUpdateView.as_view(), name='book_update'),
     path('book/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book_delete'),
+    path('author/new/', views.AuthorCreateView.as_view(), name='author-create'),
+    path('genre/new/', views.GenreCreateView.as_view(), name='genre-create'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('book/<int:pk>/update/', views.BookUpdateView.as_view(), name='book_update'),
 ]
--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic, View
-from .models import Book
+from .models import Book, Genre, Author
 from django.http import HttpResponse
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -8,6 +8,10 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
+class GenreListView(LoginRequiredMixin, ListView):
+    model = Genre
+    template_name = 'books/genre_list.html'
+
 class BookListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = 'books/book_list.html'
@@ -20,6 +24,13 @@ class BookCreateView(LoginRequiredMixin, CreateView):
     model = Book
     fields = ['title', 'author', 'genres']
     template_name = 'books/book_form.html'
+    
+    def get_initial(self):
+        initial = super().get_initial()
+        genre_id = self.request.GET.get('genre')
+        if genre_id:
+            initial['genres'] = [Genre.objects.get(id=genre_id)]
+        return initial
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
@@ -34,6 +45,16 @@ class BookDeleteView(LoginRequiredMixin, DeleteView):
     model = Book
     template_name = 'books/book_confirm_delete.html'
     success_url = '/' # Redirect after delete
+
+class AuthorCreateView(LoginRequiredMixin, CreateView):
+    model = Author
+    fields = ['name']
+    template_name = 'books/author_form.html'
+
+class GenreCreateView(LoginRequiredMixin, CreateView):
+    model = Genre
+    fields = ['name']
+    template_name = 'books/genre_form.html'
 
 class UserLogoutView(LogoutView):
     template_name = 'books/logout.html'
